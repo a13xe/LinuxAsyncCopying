2edc52d238d62c91a3a68b0f8ce1eb7c55baf71b
a13xe <77492646+a13xe@users.noreply.github.com>
2023-12-11 03:59:01 +0300

    Update async_copy.c
    remove `wait_for_aio_operations` func

--- async_copy.c
+++ async_copy.c
@@ -6,6 +6,7 @@
 #include <string.h>
 #include <aio.h>
 #include <time.h>
+#include <sys/stat.h>
 
 // Define the maximum number of I/O operations that can be pending at the same time.
 #define MAX_IO_OPERATIONS 64
@@ -47,26 +48,6 @@ void aio_write_setup(struct aiocb *aiocbp, int fd, off_t offset, volatile void *
 }
 
 // -------------------------------------------------------------------------------------------------------
-// Function to wait for all asynchronous I/O operations to complete.
-// -------------------------------------------------------------------------------------------------------
-void wait_for_aio_operations(struct aiocb *aiocbp_list, int num_ops)
-{
-    for (int i = 0; i < num_ops; i++) 
-    {
-        while (aio_error(&aiocbp_list[i]) == EINPROGRESS) 
-        {
-            usleep(1);
-        }
-        int ret = aio_return(&aiocbp_list[i]);
-        if (ret == -1) 
-        {
-            perror("aio_return");
-            exit(EXIT_FAILURE);
-        }
-    }
-}
-
-// -------------------------------------------------------------------------------------------------------
 // MAIN Function
 // -------------------------------------------------------------------------------------------------------
 int main()
@@ -77,6 +58,8 @@ int main()
     int num_async_ops;
     int source_fd, destination_fd;
     off_t offset = 0;
+    struct stat source_stat; // for getting file size
+    off_t total_size; // total size of the source file
     ssize_t bytes_read, bytes_written;
     struct timespec start, finish;
     double elapsed;
@@ -121,10 +104,19 @@ int main()
         perror("open source");
         exit(EXIT_FAILURE);
     }
+    // Get the size of the source file
+    if (fstat(source_fd, &source_stat) == -1) 
+    {
+        perror("fstat");
+        close(source_fd);
+        exit(EXIT_FAILURE);
+    }
+    total_size = source_stat.st_size;
     destination_fd = open(destination_path, O_WRONLY | O_CREAT, 0644);
     if (destination_fd == -1) 
     {
         perror("open destination");
+        close(source_fd);
         exit(EXIT_FAILURE);
     }
 
@@ -134,68 +126,68 @@ int main()
     if (!buffer) 
     {
         perror("malloc");
+        close(source_fd);
+        close(destination_fd);
         exit(EXIT_FAILURE);
     }
 
-    // Start timing the copy operation.
     clock_gettime(CLOCK_MONOTONIC, &start);
-
-    // Set up and initiate the first batch of read operations.
+    // Start the initial read operations
     for (int i = 0; i < num_async_ops; i++) 
     {
         aio_read_setup(&aiocb_list[i], source_fd, offset, buffer + (i * block_size), block_size);
-        offset += block_size; // Increment the file offset for the next operation.
+        offset += block_size;
     }
 
-    // Loop to handle the asynchronous read and write operations.
-    // -------------------------------------------------------------------------------------------------------
-    int ops_in_progress = num_async_ops; // Keep track of the number of operations in progress.
+    int ops_in_progress = num_async_ops;
     while (ops_in_progress > 0) 
     {
-        wait_for_aio_operations(aiocb_list, num_async_ops); // Wait for the read operations to complete.
-        for (int i = 0; i < num_async_ops; i++) {
-            bytes_read = aio_return(&aiocb_list[i]); // Check how many bytes were read.
-            if (bytes_read > 0) 
+        for (int i = 0; i < num_async_ops; i++) 
+        {
+            int ret = aio_error(&aiocb_list[i]);
+            if (ret == EINPROGRESS) continue;
+
+            if (aiocb_list[i].aio_lio_opcode == LIO_READ) 
             {
-                // Set up and initiate the write operation corresponding to the read operation that just completed.
-                aio_write_setup(&aiocb_list[i], destination_fd, aiocb_list[i].aio_offset, (void *)aiocb_list[i].aio_buf, bytes_read);
+                bytes_read = aio_return(&aiocb_list[i]);
+                if (bytes_read > 0) 
+                {
+                    // Set up and initiate the write operation corresponding to the read operation that just completed.
+                    aio_write_setup(&aiocb_list[i], destination_fd, aiocb_list[i].aio_offset, (void *)aiocb_list[i].aio_buf, bytes_read);
+                } 
+                else 
+                {
+                    // If read returned 0, we're at the end of the file, so decrement the operations count.
+                    ops_in_progress--;
+                }
             } 
-            else 
+            else if (aiocb_list[i].aio_lio_opcode == LIO_WRITE) 
             {
-                // If read returned 0, we're at the end of the file, so decrement the operations count.
-                ops_in_progress--;
-            }
-        }
-        wait_for_aio_operations(aiocb_list, num_async_ops); // Wait for the write operations to complete.
-        for (int i = 0; i < num_async_ops; i++) {
-            bytes_written = aio_return(&aiocb_list[i]); // Check how many bytes were written.
-            if (bytes_written > 0) 
-            {
-                // If there's more data to read, set up the next read operation.
-                aio_read_setup(&aiocb_list[i], source_fd, offset, buffer + (i * block_size), block_size);
-                offset += block_size; // Increment the file offset for the next read operation.
+                bytes_written = aio_return(&aiocb_list[i]);
+                if (bytes_written > 0 && offset < total_size) 
+                {
+                    // If there's more data to read, set up the next read operation.
+                    aio_read_setup(&aiocb_list[i], source_fd, offset, buffer + (i * block_size), block_size);
+                    offset += block_size;
+                } 
+                else 
+                {
+                    // If read returned 0, we're at the end of the file, so decrement the operations count.
+                    ops_in_progress--;
+                }
             }
         }
     }
 
-    // Stop timing the copy operation.
     clock_gettime(CLOCK_MONOTONIC, &finish);
-
-    // Calculate the total time taken for the copy operation.
     elapsed = (finish.tv_sec - start.tv_sec);
     elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
 
-    // Clean up: close the file descriptors and free the allocated buffer.
     close(source_fd);
     close(destination_fd);
     free(buffer);
 
-    // Green text
-    printf("\033[0;32;40m");
-    // Print the result of the copy operation.
     printf("\nCopying was completed in %f seconds\n", elapsed);
-    // Normal text color
-    printf("\033[0m");
 
     return 0;
 }
